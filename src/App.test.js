521dc92531036e168f7dade2f6e8c55e1253c1c7
rok93 <goodboy302@naver.com>
2021-07-22 21:26:47 +0900

    test: 화면에 설명문이 잘 표시되는지 확인하기 위해 테스트 명세 수정

--- src/App.test.js
+++ src/App.test.js
@@ -11,12 +11,16 @@ describe('<App />', () => {
   it('renders component correctly', () => {
     const {container} = render(<App/>);
     expect(container.getElementsByClassName('App-logo')).toHaveLength(1);
-    expect(container.getElementsByClassName('App-logo')[0]).toHaveAttribute(
+    expect(container.getElementsByClassName('App-logo')[0])
+    .toHaveAttribute(
         'src', 'logo.svg'
     )
+    expect(container.getElementsByTagName('p')).toHaveLength(1);
+    expect(container.getElementsByTagName('p')[0]).toHaveTextContent(
+        'Edit src/App.js and save to reload.'
+    );
 
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
   });
-
-})
+});
