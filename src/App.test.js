0034c50a781cfa7f8b181341cd5ae6bca1267a49
rok93 <goodboy302@naver.com>
2021-07-22 21:12:13 +0900

    test: 이미지가 잘 표시되는지 확인하기 위해 테스트 코드 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders learn react link', () => { // test는 Jest의 함수이고 it 함수와 같은 역할을 한다!
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('<App />', () => {
+  it('renders component correctly', () => {
+    const {container} = render(<App/>);
+    expect(container.getElementsByClassName('App-logo')).toHaveLength(1);
+    expect(container.getElementsByClassName('App-logo')[0]).toHaveAttribute(
+        'src', 'logo.svg'
+    )
+
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+})
